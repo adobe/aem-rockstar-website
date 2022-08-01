27675d301db2163c39a27a78d2b014c8a2054bf2
Sean Steimer <ssteimer@adobe.com>
2022-08-01 16:51:40 -0700

    style tweaking

--- blocks/winners-carousel/winners-carousel.js
+++ blocks/winners-carousel/winners-carousel.js
@@ -65,12 +65,21 @@ export default async function decorate(block) {
       if (child.querySelector('picture')) {
         child.classList.add('carousel-slide-card-image');
       } else {
+        // strip out unwanted p tags
         child.innerHTML = child.innerHTML.replace(/(<p[^>]+?>|<p>|<\/p>)/img, '');
         slideContent.appendChild(child);
       }
     });
 
     slide.appendChild(slideContent);
+  });
+
+  // remove button classes from links
+  document.querySelectorAll('.carousel-slide-card-content div > a').forEach((btn) => {
+    btn.classList.remove('button');
+    btn.classList.remove('primary');
+
+    btn.parentElement.classList.remove('button-container');
   });
 
   // setup for multiple slides
