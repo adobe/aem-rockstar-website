5f28627a5b23599438877c13571cc62f9c987c35
Sean Steimer <ssteimer@adobe.com>
2022-09-29 08:33:31 -0700

    fixing sidekick config, remove scheme from host

--- tools/sidekick/config.js
+++ tools/sidekick/config.js
@@ -12,7 +12,7 @@ governing permissions and limitations under the License.
 (() => {
   window.hlx.initSidekick({
     project: 'AEM Rockstar Website',
-    host: 'https://rockstar.adobeevents.com',
+    host: 'rockstar.adobeevents.com',
     pushDown: true,
     plugins: [],
   });
