08c64a11f35fb2128b40033328036e3e5147f497
Darin Kuntze <dkuntze@gmail.com>
2023-10-24 12:13:16 -0500

    upgraaaaade

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -10,7 +10,7 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import { readBlockConfig, decorateIcons } from '../../scripts/scripts.js';
+import { readBlockConfig, decorateIcons } from '../../scripts/aem.js';
 import { event } from '../../scripts/instrument.js';
 
 function decorateBackToTop(element) {
